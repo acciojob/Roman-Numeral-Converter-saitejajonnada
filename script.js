4b15d2add2724fde9ac63591739ef90b4435e5d1
Jonnada Sai Teja <146433770+saitejajonnada@users.noreply.github.com>
2025-03-28 18:57:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function convertToRoman(num) {
-  	const obj = {
+  	const symbol = {
       0:['M',1000], 
       1:['D', 500], 
       2:['C', 100], 
@@ -9,31 +9,19 @@ function convertToRoman(num) {
       6:['I', 1]
     };
 	let roman = "";
-    
-    for (let i = 0; i <= 6; i += 2) {  
-        let symbol = obj[i][0];
-        let value = obj[i][1];
-        let nextSymbol = obj[i + 2] ? obj[i + 2][0] : null;
-        let nextValue = obj[i + 2] ? obj[i + 2][1] : null;
-        
-        if (nextValue && num >= value - nextValue) {
-            roman += nextSymbol + symbol;
-            num -= (value - nextValue);
-        }
-        while (num >= value) {
-            roman += symbol;
-            num -= value;
-        }
-        if (num >= obj[i + 1][1]) {  
-            roman += obj[i + 1][0];
-            num -= obj[i + 1][1];
-        }
-    }
-    
+	symbols.forEach((item)=>{
+		const[symbol,value] = item;
+		while(num >= value){
+			roman+=symbol;
+			num -= value;
+		}
+	})
     return roman;
 }
+console.log(convertToRoman(14));
+console.log(convertToRoman(798));
+
 
 console.log(convertToRoman(36)); // Output: XXXVI
-
 module.exports = convertToRoman;
   
